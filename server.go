ada376b3ffe02199dd0292b7934c88492cd9406a
MKondakova <milanon@ya.ru>
2021-04-09 14:18:30 +0300

    обновила интерфейсы, написала userUsercase

--- server.go
+++ server.go
@@ -59,13 +59,9 @@ func Router(e *echo.Echo) {
 
 //fixme вынести в соотв delivery
 func Router(e *echo.Echo) {
-	e.POST("/api/users/", registration)
 	e.POST("/api/login/", login)
 	e.DELETE("/api/logout/", logout)
-	e.GET("/api/users/", GetCurrentUser)
-	e.GET("/api/users/:id", GetUserByID)
-	e.PATCH("/api/users/", PatchUser)
-	e.DELETE("/api/users/:id", DeleteUserByID)
+	e.POST("/api/users/", registration)
 	e.POST("/api/upload/", upload)
 	e.GET("/static/:filename", download)
 	e.GET("/api/authorized/", IsAuthorized)
