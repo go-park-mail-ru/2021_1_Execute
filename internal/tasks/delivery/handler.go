5474ac112331e0a79f911e18eb09d83a8753b4eb
AlexisOMG <alesha-gramazeka@yandex.ru>
2021-04-13 12:56:45 +0300

    Хэндлер тасок

--- internal/tasks/delivery/handler.go
+++ internal/tasks/delivery/handler.go
@@ -1,8 +1,24 @@
 package delivery
 
-import "2021_1_Execute/internal/domain"
+import (
+	"2021_1_Execute/internal/domain"
+
+	"github.com/labstack/echo"
+)
 
 type TasksHandler struct {
 	sessionHD domain.SessionHandler
 	taskUC    domain.TaskUsecase
+}
+
+func NewTasksHandler(e *echo.Echo, sessionHD domain.SessionHandler, taskUC domain.TaskUsecase) {
+	handler := &TasksHandler{
+		sessionHD: sessionHD,
+		taskUC:    taskUC,
+	}
+
+	e.GET("api/tasks/:id", handler.GetTask)
+	e.POST("api/tasks", handler.PostTask)
+	e.PATCH("api/tasks/:id", handler.PatchTask)
+	e.DELETE("api/tasks/:id", handler.DeleteTask)
 }
